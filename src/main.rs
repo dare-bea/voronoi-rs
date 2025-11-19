b05e49b8244a4099acd7464940014e210c19fd85
dare-bea <twopenaboys@gmail.com>
2025-11-19 18:46:22 -0500

    Format

--- src/main.rs
+++ src/main.rs
@@ -153,11 +153,13 @@ fn main() {
                 }
             }
 
-            if let Some(radius) = args.point_radius && {
-                let dx = x.abs_diff(min_pos.0);
-                let dy = y.abs_diff(min_pos.1);
-                dx * dx + dy * dy < radius * (radius - 1)
-            } {
+            if let Some(radius) = args.point_radius
+                && {
+                    let dx = x.abs_diff(min_pos.0);
+                    let dy = y.abs_diff(min_pos.1);
+                    dx * dx + dy * dy < radius * (radius - 1)
+                }
+            {
                 min_color = min_color.map(|c| u8::MAX - c);
             }
 
